baab20e543bd7877bfffd6985be19d0a7aa828fe
Oleg <pride96341@gmail.com>
2021-02-28 11:13:31 +0300

    изменил код

--- ymcparser/ymcparser.go
+++ ymcparser/ymcparser.go
@@ -30,9 +30,10 @@ func Strart() {
 
 	// Find and visit all links
 	c.OnHTML(".location-list-row", func(e *colly.HTMLElement) {
-		//e.Request.Visit(e.Attr("href"))
+		e.Request.Visit(e.Attr("href"))
 		e.ForEach(".location-list-item", func(i int, e1 *colly.HTMLElement) {
-			fmt.Println("I=", i, "ANSW", *e1)
+			//	fmt.Println("I=", i, "ANSW", *e1)
+			e.Request.Ctx.Put(i, *e1)
 
 		})
 		fmt.Println("END")
@@ -41,7 +42,6 @@ func Strart() {
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
-		colly.Request.Ctx
 
 	})
 
